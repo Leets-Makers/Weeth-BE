2dd44ae654f6e02a372be6d0ee3cb4ef38ab1a2a
hyxklee <ewgt1234@naver.com>
2025-05-02 14:34:37 +0900

    feat: AbstractOAuth2Token redis 저장용 커스텀 객체 구현

---
+++ src/main/java/leets/weeth/global/sas/domain/entity/AuthorizationCode.java
@@ -0,0 +1,12 @@
+package leets.weeth.global.sas.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+@AllArgsConstructor
+public class AuthorizationCode extends AbstractToken {
+
+}
