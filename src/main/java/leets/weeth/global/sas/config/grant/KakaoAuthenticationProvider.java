649ff9ca3440fa57bdd4b019105196082db9f5b1
hyxklee <ewgt1234@naver.com>
2025-05-10 22:41:30 +0900

    feat: 커스텀 grant type 추상 클래스 및 구현 클래스 작성

---
+++ src/main/java/leets/weeth/global/sas/config/grant/KakaoAuthenticationProvider.java
@@ -0,0 +1,61 @@
+package leets.weeth.global.sas.config.grant;
+
+import leets.weeth.domain.user.domain.entity.User;
+import leets.weeth.domain.user.domain.service.UserGetService;
+import leets.weeth.global.auth.kakao.KakaoAuthService;
+import leets.weeth.global.auth.kakao.dto.KakaoUserInfoResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2ErrorCodes;
+import org.springframework.security.oauth2.core.OAuth2Token;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenGenerator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KakaoAuthenticationProvider extends CustomAuthenticationProvider<KakaoUserInfoResponse> {
+
+    private final KakaoAuthService kakaoAuthService;
+    private final UserGetService userGetService;
+
+    public KakaoAuthenticationProvider(
+            KakaoAuthService kakaoAuthService,
+            UserGetService userGetService,
+            OAuth2AuthorizationService authorizationService,
+            OAuth2TokenGenerator<? extends OAuth2Token> tokenGenerator
+    ) {
+        super(authorizationService, tokenGenerator);
+        this.kakaoAuthService = kakaoAuthService;
+        this.userGetService = userGetService;
+    }
+
+    @Override
+    protected AuthorizationGrantType getGrantTokenType() {
+        return KakaoGrantType.KAKAO_ACCESS_TOKEN;
+    }
+
+    @Override
+    protected Class<? extends Authentication> getAuthenticationClass() {
+        return KakaoAccessTokenAuthenticationToken.class;
+    }
+
+    @Override
+    protected String extractAccessToken(Authentication authentication) {
+        KakaoAccessTokenAuthenticationToken grantAuth = (KakaoAccessTokenAuthenticationToken) authentication;
+
+        return grantAuth.getKakaoAccessToken();
+    }
+
+    @Override
+    protected KakaoUserInfoResponse getUserInfo(String accessToken) {
+        return kakaoAuthService.getUserInfo(accessToken);
+    }
+
+    @Override
+    protected User getOrLoadUser(KakaoUserInfoResponse userInfo) {
+        long kakaoId = userInfo.id();
+        return userGetService.find(kakaoId)
+                .orElseThrow(() -> new OAuth2AuthenticationException(OAuth2ErrorCodes.INVALID_GRANT));
+    }
+}
