c50835ba3b92a31b42aa66f25c9703eeb03df600
hyxklee <ewgt1234@naver.com>
2025-05-10 22:42:34 +0900

    feat: authMapper 구현

---
+++ src/main/java/leets/weeth/global/sas/application/mapper/OauthMapper.java
@@ -0,0 +1,14 @@
+package leets.weeth.global.sas.application.mapper;
+
+import leets.weeth.domain.user.domain.entity.User;
+import leets.weeth.global.sas.application.dto.OauthUserInfoResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface OauthMapper {
+
+    OauthUserInfoResponse toResponse(User user, int cardinal);
+}
+
