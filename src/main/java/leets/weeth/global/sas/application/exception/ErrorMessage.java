e21bcaf20aacf372285a0e7d23a7bc25fd709741
hyxklee <ewgt1234@naver.com>
2025-05-17 15:06:35 +0900

    refactor: 예외 메시지 수정

--- src/main/java/leets/weeth/global/sas/application/exception/ErrorMessage.java
+++ src/main/java/leets/weeth/global/sas/application/exception/ErrorMessage.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public enum ErrorMessage {
 
     USER_INACTIVE("WAE-001", "가입 승인이 허가되지 않은 계정입니다."),
-    USER_NOT_FOUND("WAE-002", "해당 유저를 찾을 수 없습니다.");
+    USER_NOT_FOUND("WAE-002", "존재하지 않는 유저입니다.");
 
     private final String code;
     private final String description;
