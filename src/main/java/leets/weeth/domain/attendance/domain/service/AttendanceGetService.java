c62c589ebd156d683acfaf669f9a0ee3f23742f1
hyxklee <ewgt1234@naver.com>
2025-05-29 16:45:03 +0900

    refactor: 현재 활동 중인 인원의 출결 기록만 보이도록 수정

--- src/main/java/leets/weeth/domain/attendance/domain/service/AttendanceGetService.java
+++ src/main/java/leets/weeth/domain/attendance/domain/service/AttendanceGetService.java
@@ -4,6 +4,7 @@ import leets.weeth.domain.attendance.application.exception.AttendanceNotFoundExc
 import leets.weeth.domain.attendance.domain.entity.Attendance;
 import leets.weeth.domain.attendance.domain.repository.AttendanceRepository;
 import leets.weeth.domain.schedule.domain.entity.Meeting;
+import leets.weeth.domain.user.domain.entity.enums.Status;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,7 @@ public class AttendanceGetService {
     private final AttendanceRepository attendanceRepository;
 
     public List<Attendance> findAllByMeeting(Meeting meeting) {
-        return attendanceRepository.findAllByMeeting(meeting);
+        return attendanceRepository.findAllByMeetingAndUserStatus(meeting, Status.ACTIVE);
     }
     public Attendance findByAttendanceId(Long attendanceId) {
         return attendanceRepository.findById(attendanceId)
