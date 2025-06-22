ee1fbffd7d972125651ae59619edf88576c38f6d
hyxklee <ewgt1234@naver.com>
2025-06-22 22:49:13 +0900

    refactor: 유저 정보 반환시 파트도 함께 반환

--- src/main/java/leets/weeth/global/sas/application/dto/OauthUserInfoResponse.java
+++ src/main/java/leets/weeth/global/sas/application/dto/OauthUserInfoResponse.java
@@ -1,10 +1,13 @@
 package leets.weeth.global.sas.application.dto;
 
+import leets.weeth.domain.user.domain.entity.enums.Position;
+
 public record OauthUserInfoResponse(
         long userId,
         String name,
         String email,
         String tel,
+        Position position,
         int cardinal
 ) {
 }
