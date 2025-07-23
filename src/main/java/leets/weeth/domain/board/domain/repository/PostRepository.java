6e6bc2aacaa533d0f942f6d5f211b44307e5e526
huncozyboy <leejh5838@naver.com>
2025-07-24 08:47:34 +0900

    feat : 파트별 게시글 조회 메서드 추가

--- src/main/java/leets/weeth/domain/board/domain/repository/PostRepository.java
+++ src/main/java/leets/weeth/domain/board/domain/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package leets.weeth.domain.board.domain.repository;
 
 import leets.weeth.domain.board.domain.entity.Post;
+import leets.weeth.domain.board.domain.entity.enums.PostPart;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +10,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 	Slice<Post> findPageBy(Pageable page);
+
+	Slice<Post> findPageByPostPart(PostPart postPart, Pageable pageable);
 
 	Slice<Post> findByTitleContainingOrContentContainingIgnoreCase(String keyword1, String keyword2, Pageable pageable);
 }
