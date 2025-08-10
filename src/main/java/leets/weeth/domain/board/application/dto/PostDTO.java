9581c18bb5126f514b890c28aa7e104b06851ad4
hyxklee <ewgt1234@naver.com>
2025-08-10 13:46:50 +0900

    fix: 게시글 상세 조회시 생성 날짜로 반환

--- src/main/java/leets/weeth/domain/board/application/dto/PostDTO.java
+++ src/main/java/leets/weeth/domain/board/application/dto/PostDTO.java
@@ -2,8 +2,6 @@ package leets.weeth.domain.board.application.dto;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-import java.time.LocalDateTime;
-import java.util.List;
 import leets.weeth.domain.board.domain.entity.enums.Category;
 import leets.weeth.domain.board.domain.entity.enums.Part;
 import leets.weeth.domain.comment.application.dto.CommentDTO;
@@ -12,6 +10,9 @@ import leets.weeth.domain.file.application.dto.response.FileResponse;
 import leets.weeth.domain.user.domain.entity.enums.Position;
 import leets.weeth.domain.user.domain.entity.enums.Role;
 import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class PostDTO {
 
@@ -48,14 +49,15 @@ public class PostDTO {
             Role role,
             String title,
             String content,
-            LocalDateTime time,//modifiedAt
+            LocalDateTime time, //createdAt
             Integer commentCount,
             List<CommentDTO.Response> comments,
             List<FileResponse> fileUrls
     ){}
 
     @Builder
-    public record ResponseAll(
+    public record
+    ResponseAll(
             Long id,
             String name,
             Position position,
