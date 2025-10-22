4041f1e2b9eb70df18952cbd04dd821a0f0fd238
jj <syeonie0526@gmail.com>
2025-10-22 20:47:23 +0900

    test: notice 검색 테스트 코드 작성

--- src/test/java/leets/weeth/domain/board/domain/repository/NoticeRepositoryTest.java
+++ src/test/java/leets/weeth/domain/board/domain/repository/NoticeRepositoryTest.java
@@ -87,5 +87,54 @@ class NoticeRepositoryTest {
 
     @Test
     void search() {
+        // given
+        User user = userRepository.save(User.builder()
+                .email("abc@test.com")
+                .name("홍길동")
+                .position(Position.BE)
+                .department(Department.SW)
+                .role(Role.USER)
+                .build());
+
+        Notice notice1 = noticeRepository.save(Notice.builder()
+                .title("검색1")
+                .content("내용1")
+                .user(user)
+                .build());
+
+        Notice notice2 = noticeRepository.save(Notice.builder()
+                .title("제목2")
+                .content("내용2")
+                .user(user)
+                .build());
+
+        Notice notice3 = noticeRepository.save(Notice.builder()
+                .title("검색3")
+                .content("내용3")
+                .user(user)
+                .build());
+
+        Notice notice4 = noticeRepository.save(Notice.builder()
+                .title("제목4")
+                .content("내용4")
+                .user(user)
+                .build());
+
+        Notice notice5 = noticeRepository.save(Notice.builder()
+                .title("검색5")
+                .content("내용5")
+                .user(user)
+                .build());
+
+        Pageable pageable = PageRequest.of(0, 2, Sort.by(Sort.Direction.DESC, "id"));
+
+        // when
+        Slice<Notice> searchedNotices = noticeRepository.search("검색", pageable);
+
+        // then
+        assertThat(searchedNotices.getSize()).isEqualTo(2);
+        assertThat(searchedNotices.getContent().get(0).getTitle()).isEqualTo(notice5.getTitle());
+        assertThat(searchedNotices.getContent().get(1).getTitle()).isEqualTo(notice3.getTitle());
+        assertThat(searchedNotices.hasNext()).isEqualTo(true);
     }
 }
