5dba9bdab43924f7719d679b4166e79025c32774
jj <syeonie0526@gmail.com>
2025-10-25 20:13:41 +0900

    chore: /actuator/health 엔드포인트 접근 허용 설정 추가

--- src/main/java/leets/weeth/global/config/SecurityConfig.java
+++ src/main/java/leets/weeth/global/config/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig {
                                                 "/js/**", "/img/**", "/scss/**", "/vendor/**").permitAll()
                                         // 스웨거 경로
                                         .requestMatchers("/v3/api-docs", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/swagger/**").permitAll()
-                                        .requestMatchers("/actuator/prometheus").permitAll()
+                                        .requestMatchers("/actuator/health", "actuator/prometheus").permitAll()
                                         .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                                         .anyRequest().authenticated()
                 )
