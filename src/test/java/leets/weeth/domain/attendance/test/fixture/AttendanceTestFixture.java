6c592e4401945f523e1f84fc2b957225c8faa7a4
huncozyboy <leejh5838@naver.com>
2025-10-31 10:46:18 +0900

    test : 미사용 메서드 삭제

--- src/test/java/leets/weeth/domain/attendance/test/fixture/AttendanceTestFixture.java
+++ src/test/java/leets/weeth/domain/attendance/test/fixture/AttendanceTestFixture.java
@@ -67,16 +67,6 @@ public class AttendanceTestFixture {
 			.build();
 	}
 
-	private static void set(Object target, String fieldName, Object value) {
-		try {
-			Field f = target.getClass().getDeclaredField(fieldName);
-			f.setAccessible(true);
-			f.set(target, value);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
 	private static void setField(Object target, String fieldName, Object value) {
 		try {
 			Field field = target.getClass().getDeclaredField(fieldName);
