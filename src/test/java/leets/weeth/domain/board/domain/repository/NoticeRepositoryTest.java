c744d9b15e3675c13e4f56bf210e2041cc9acf3d
jj <syeonie0526@gmail.com>
2025-10-31 18:57:52 +0900

    refactor: 공지 레포지터리 테스트 수정 - NoticeFixture 사용하는 반복문으로 수정 - 메서드명 테스트 목적이 포함되도록 수정 - 검증 로직 수정(id 내림차순 조회 및 검색 결과 검증)

--- src/test/java/leets/weeth/domain/board/domain/repository/NoticeRepositoryTest.java
+++ src/test/java/leets/weeth/domain/board/domain/repository/NoticeRepositoryTest.java
@@ -2,9 +2,8 @@ package leets.weeth.domain.board.domain.repository;
 
 import leets.weeth.config.TestContainersConfig;
 import leets.weeth.domain.board.domain.entity.Notice;
-import leets.weeth.domain.user.domain.service.UserSaveService;
+import leets.weeth.domain.board.domain.test.fixture.NoticeFixture;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,6 +12,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
@@ -25,83 +27,61 @@ class NoticeRepositoryTest {
     private NoticeRepository noticeRepository;
 
     @Test
-    void findPageBy() {
+    void findPageBy_공지_id_내림차순으로_조회() {
         // given
-        Notice notice1 = noticeRepository.save(Notice.builder()
-                .title("제목1")
-                .content("내용1")
-                .build());
+        List<Notice> notices = new ArrayList<>();
+        for(int i = 0; i<5; i++){
+            Notice notice = NoticeFixture.createNotice("공지" + i);
+            notices.add(notice);
+        }
 
-        Notice notice2 = noticeRepository.save(Notice.builder()
-                .title("제목2")
-                .content("내용2")
-                .build());
-
-        Notice notice3 = noticeRepository.save(Notice.builder()
-                .title("제목3")
-                .content("내용3")
-                .build());
-
-        Notice notice4 = noticeRepository.save(Notice.builder()
-                .title("제목4")
-                .content("내용4")
-                .build());
-
-        Notice notice5 = noticeRepository.save(Notice.builder()
-                .title("제목5")
-                .content("내용5")
-                .build());
-
+        noticeRepository.saveAll(notices);
         Pageable pageable = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "id"));
 
         // when
-        Slice<Notice> notices = noticeRepository.findPageBy(pageable);
+        Slice<Notice> pagedNotices  = noticeRepository.findPageBy(pageable);
 
         // then
-        assertThat(notices.getSize()).isEqualTo(3);
-        assertThat(notices.getContent().get(0).getTitle()).isEqualTo(notice5.getTitle());
-        assertThat(notices.getContent().get(1).getTitle()).isEqualTo(notice4.getTitle());
-        assertThat(notices.getContent().get(2).getTitle()).isEqualTo(notice3.getTitle());
-        assertThat(notices.hasNext()).isEqualTo(true);
+        assertThat(pagedNotices .getSize()).isEqualTo(3);
+        assertThat(pagedNotices)
+                .extracting(Notice::getTitle)
+                        .containsExactly(
+                                notices.get(4).getTitle(),
+                                notices.get(3).getTitle(),
+                                notices.get(2).getTitle()
+                        );
+        assertThat(pagedNotices .hasNext()).isTrue();
     }
 
     @Test
-    void search() {
+    void search_검색어가_포함된_공지_id_내림차순으로_조회() {
         // given
-        Notice notice1 = noticeRepository.save(Notice.builder()
-                .title("검색1")
-                .content("내용1")
-                .build());
+        List<Notice> notices = new ArrayList<>();
+        for(int i = 0; i<6; i++){
+            Notice notice;
+            if(i % 2 == 0){
+                notice = NoticeFixture.createNotice("공지" + i);
+            } else{
+                notice = NoticeFixture.createNotice("검색" + i);
+            }
+            notices.add(notice);
+        }
 
-        Notice notice2 = noticeRepository.save(Notice.builder()
-                .title("제목2")
-                .content("내용2")
-                .build());
+        noticeRepository.saveAll(notices);
 
-        Notice notice3 = noticeRepository.save(Notice.builder()
-                .title("검색3")
-                .content("내용3")
-                .build());
-
-        Notice notice4 = noticeRepository.save(Notice.builder()
-                .title("제목4")
-                .content("내용4")
-                .build());
-
-        Notice notice5 = noticeRepository.save(Notice.builder()
-                .title("검색5")
-                .content("내용5")
-                .build());
-
-        Pageable pageable = PageRequest.of(0, 2, Sort.by(Sort.Direction.DESC, "id"));
+        Pageable pageable = PageRequest.of(0, 5, Sort.by(Sort.Direction.DESC, "id"));
 
         // when
         Slice<Notice> searchedNotices = noticeRepository.search("검색", pageable);
 
         // then
-        assertThat(searchedNotices.getSize()).isEqualTo(2);
-        assertThat(searchedNotices.getContent().get(0).getTitle()).isEqualTo(notice5.getTitle());
-        assertThat(searchedNotices.getContent().get(1).getTitle()).isEqualTo(notice3.getTitle());
-        assertThat(searchedNotices.hasNext()).isEqualTo(true);
+        assertThat(searchedNotices.getContent()).hasSize(3);
+        assertThat(searchedNotices.getContent())
+                .extracting(Notice::getTitle)
+                        .containsExactly(notices.get(5).getTitle(),
+                                notices.get(3).getTitle(),
+                                notices.get(1).getTitle());
+        assertThat(searchedNotices.hasNext()).isFalse();
+
     }
 }
