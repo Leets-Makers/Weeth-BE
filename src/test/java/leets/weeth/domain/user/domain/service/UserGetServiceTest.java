27acd569ae877bfb21085d43dc252313e87b175a
seokjun01 <suckjungod@naver.com>
2025-11-02 19:19:46 +0900

    add : findAll(Pageable pageable) 빈 슬라이스 반환 시, 유저 예외 처리 검증

--- src/test/java/leets/weeth/domain/user/domain/service/UserGetServiceTest.java
+++ src/test/java/leets/weeth/domain/user/domain/service/UserGetServiceTest.java
@@ -1,6 +1,7 @@
 package leets.weeth.domain.user.domain.service;
 
 import leets.weeth.domain.user.application.exception.UserNotFoundException;
+import leets.weeth.domain.user.domain.entity.User;
 import leets.weeth.domain.user.domain.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,9 +9,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.data.domain.SliceImpl;
 
+import java.util.List;
 import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -44,6 +52,22 @@ class UserGetServiceTest {
 
 		//when & then
 		assertThrows(UserNotFoundException.class, () -> userGetService.find(email));
+	}
+
+	@Test
+	@DisplayName("findAll(Pageable pageable) : 빈 슬라이스 반환 시 , 유저 예외 던진다")
+	void findAll_userNotFound_throwsException() {
+		//given
+		Pageable pageable = PageRequest.of(0, 10);
+		Slice<User> emptySlice = new SliceImpl<>(List.of(), pageable, false);
+
+		given(userRepository.findAllByStatusOrderedByCardinalAndName(any(), eq(pageable)))
+			.willReturn(emptySlice);
+
+		//when & then
+		assertThrows(UserNotFoundException.class,
+			() -> userGetService.findAll(pageable));
+
 	}
 
 }
