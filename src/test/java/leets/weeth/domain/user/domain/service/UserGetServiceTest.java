a821efb859d830ec28e06501c93e70ae9fca048f
seokjun01 <suckjungod@naver.com>
2025-11-02 19:07:02 +0900

    feat : UserGetServiceTest 파일 생성 및 find 메소드 예외처리 검증

---
+++ src/test/java/leets/weeth/domain/user/domain/service/UserGetServiceTest.java
@@ -0,0 +1,38 @@
+package leets.weeth.domain.user.domain.service;
+
+import leets.weeth.domain.user.application.exception.UserNotFoundException;
+import leets.weeth.domain.user.domain.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.Optional;
+import static org.mockito.BDDMockito.given;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+class UserGetServiceTest {
+
+	@Mock
+	private UserRepository userRepository;
+
+	@InjectMocks
+	private UserGetService userGetService;
+
+	@Test
+	@DisplayName("find(Long Id) : 존재하지 않는 유저일 때 예외를 던진다")
+	void find_userNotFound_throwsException() {
+		//given
+		Long userId = Long.valueOf(1L);
+		given(userRepository.findById(userId)).willReturn(Optional.empty());
+
+		// when & then
+		assertThrows(UserNotFoundException.class, () -> userGetService.find(userId));
+	}
+
+}
