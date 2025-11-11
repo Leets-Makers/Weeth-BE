35c89c4ea561589d1f0aa784484cf374087f63e2
jj <syeonie0526@gmail.com>
2025-11-11 15:53:21 +0900

    test: 게시글 픽스쳐 createEducationPost와 createResponseAll추가

--- src/test/java/leets/weeth/domain/board/test/fixture/PostTestFixture.java
+++ src/test/java/leets/weeth/domain/board/test/fixture/PostTestFixture.java
@@ -1,9 +1,14 @@
 package leets.weeth.domain.board.test.fixture;
 
+import leets.weeth.domain.board.application.dto.PostDTO;
 import leets.weeth.domain.board.domain.entity.Post;
 import leets.weeth.domain.board.domain.entity.enums.Category;
+import leets.weeth.domain.board.domain.entity.enums.Part;
+import leets.weeth.domain.user.domain.entity.enums.Role;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 public class PostTestFixture {
     public static Post createPost(Long id, String title, Category category){
@@ -14,6 +19,37 @@ public class PostTestFixture {
                 .comments(new ArrayList<>())
                 .commentCount(0)
                 .category(category)
+                .build();
+    }
+
+    public static Post createEducationPost(Long id, String title, Category category, List<Part> parts,
+                                           int cardinalNumber, int week){
+        return Post.builder()
+                .id(id)
+                .title(title)
+                .content("내용")
+                .parts(parts)
+                .cardinalNumber(cardinalNumber)
+                .week(week)
+                .commentCount(0)
+                .category(Category.Education)
+                .comments(new ArrayList<>())
+                .build();
+    }
+
+    public static PostDTO.ResponseAll createResponseAll(Post post){
+        return PostDTO.ResponseAll.builder()
+                .id(post.getId())
+                .part(post.getPart())
+                .role(Role.USER)
+                .title(post.getTitle())
+                .content(post.getContent())
+                .studyName(post.getStudyName())
+                .week(post.getWeek())
+                .time(LocalDateTime.now())
+                .commentCount(post.getCommentCount())
+                .hasFile(false)
+                .isNew(false)
                 .build();
     }
 
