d369f79864d8e3814a441953051a016f4ae0ea4e
seokjun01 <suckjungod@naver.com>
2025-11-11 21:28:31 +0900

    feat : 임시로 JPQL이 복잡한 메소드 2개 RespositoryCustom 생성

---
+++ src/main/java/leets/weeth/domain/board/domain/repository/PostRepositoryImpl.java
@@ -0,0 +1,64 @@
+package leets.weeth.domain.board.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.stereotype.Repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import leets.weeth.domain.board.domain.entity.Post;
+import leets.weeth.domain.board.domain.entity.QPost;
+import leets.weeth.domain.board.domain.entity.enums.Category;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class PostRepositoryImpl implements PostRepositoryCustom {
+
+	private final JPAQueryFactory queryFactory;
+	private final QPost post = QPost.post;
+
+	@Override
+	public Slice<Post> findRecentPart(Pageable pageable) {
+		List<Post> result = queryFactory
+			.selectFrom(post)
+			.where(post.category.in(Category.StudyLog, Category.Article))
+			.orderBy(post.id.desc())
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize() + 1)
+			.fetch();
+
+		boolean hasNext = result.size() > pageable.getPageSize();
+
+		if (hasNext) {
+			result.remove(pageable.getPageSize());
+		}
+
+		return new SliceImpl<>(result, pageable, hasNext);
+	}
+
+	@Override
+	public Slice<Post> searchPart(String kw, Pageable pageable) {
+		List<Post> result = queryFactory
+			.selectFrom(post)
+			.where(post.category.in(Category.StudyLog, Category.Article)
+				.and(post.title.containsIgnoreCase(kw)
+					.or(post.content.containsIgnoreCase(kw))
+			))
+			.orderBy(post.id.desc())
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize() + 1)
+			.fetch();
+
+		boolean hasNext = result.size() > pageable.getPageSize();
+
+		if (hasNext) {
+			result.remove(pageable.getPageSize());
+		}
+
+		return new SliceImpl<>(result, pageable, hasNext);
+ 	}
+}
