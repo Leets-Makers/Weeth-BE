4997e03f101b274f05c32872690f3a517bef2633
seokjun01 <suckjungod@naver.com>
2025-11-18 20:50:22 +0900

    add : @Query 기반 JPQL 메소드 Custom 리포지토리에 추가

--- src/main/java/leets/weeth/domain/board/domain/repository/PostRepositoryCustom.java
+++ src/main/java/leets/weeth/domain/board/domain/repository/PostRepositoryCustom.java
@@ -1,14 +1,26 @@
 package leets.weeth.domain.board.domain.repository;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.repository.query.Param;
 
 import leets.weeth.domain.board.domain.entity.Post;
+import leets.weeth.domain.board.domain.entity.enums.Category;
+import leets.weeth.domain.board.domain.entity.enums.Part;
 
 public interface PostRepositoryCustom {
 
 	Slice<Post> findRecentPart(Pageable pageable);
-
+	Slice<Post> findRecentEducation(Pageable pageable);
 	Slice<Post> searchPart(String kw,Pageable pageable);
+	Slice<Post> searchEducation(String kw, Pageable pageable);
+	List<String> findDistinctStudyNamesByPart(Part part);
+	Slice<Post> findByPartAndOptionalFilters(Part part, Category category, Integer cardinal, String studyName, Integer week, Pageable pageable);
+	Slice<Post> findByCategoryAndOptionalCardinalWithPart(String partName, Category category, Integer cardinal, Pageable pageable);
+	Slice<Post> findByCategoryAndCardinalNumberWithPart(String partName, Category category, Integer cardinal, Pageable pageable);
+	Slice<Post> findByCategoryAndCardinalInWithPart(String partName, Category category,  Collection<Integer> cardinals, Pageable pageable);
 
 }
